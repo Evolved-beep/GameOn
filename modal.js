d22ccac5a4cef6de675bf732a29e74e5a156db7c
Evolved-beep <maxime.magnier@outlook.com>
2022-07-26 14:34:52 +0200

    Ajout des erreurs et de la validation

--- modal.js
+++ modal.js
@@ -17,10 +17,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display ="block";
+  modalbg.style.display = "block";
 }
 
-// Modal element 
+// Modal element
 const closemodal = document.querySelector(".close");
 
 // Fermeture du modal event
@@ -28,153 +28,209 @@ closemodal.addEventListener("click", removeClick);
 
 //Fonction pour faire disparaître le modal au clic
 function removeClick() {
-  modalbg.style.display="none";
+  modalbg.style.display = "none";
+  reset();
 }
 
-// Variable pour récupèrer le form 
-let form = document.querySelector('#form-action');
+function reset(){
+  form.reset();
+}
+
+const modalSuccess = document.querySelector(".bgsuccess");
+
+function launchSuccess(){
+  modalSuccess.style.display="block";
+  form.submit();
+}
+
+// Variable pour récupèrer le form
+let form = document.querySelector("#form-action");
 
 // On découpe chaque input pour pouvoir y effectuer les validations nécéssaires
-form.first.addEventListener('change', function(){
-  validName(this);
+form.first.addEventListener("change", function () {
+  validName();
 });
 
-form.last.addEventListener('change', function(){
-  validFamilyName(this);
+form.last.addEventListener("change", function () {
+  validFamilyName();
 });
 
-form.email.addEventListener('change', function(){
-  validEmail(this);
+form.email.addEventListener("change", function () {
+  validEmail();
 });
-form.birthdate.addEventListener('change', function(){
-  validDate(this);
-});
-
-form.quantity.addEventListener('change', function(){
-  validTournament(this);
+form.birthdate.addEventListener("change", function () {
+  validDate();
 });
 
-form.location.forEach(radioBtn => {
-  radioBtn.addEventListener('change', function(){
-    validLocation(this);
-  })
+form.quantity.addEventListener("change", function () {
+  validTournament();
+});
+
+form.location.forEach((radioBtn) => {
+  radioBtn.addEventListener("change", function () {
+    validLocation();
+  });
 });
 
 const Condition = document.getElementById("checkbox1");
 
-Condition.addEventListener('click', function(){
-  validCondition(this);
-})
+Condition.addEventListener("click", function () {
+  validCondition();
+});
 
-form.addEventListener('submit', function(event){
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  if (validate() === true){
-    form.submit();
-
+  if (validate() === true) {
+    return launchSuccess();
   } else {
     alert("Votre formulaire ne peut pas être envoyé");
   }
+});
 
-})
+let Msg = document.createElement("span");
 
-
-// Création des fonctions afin de mettre en place la validité
-
-const validName = function(inputName){
-  // Création de la regex pour les inputs nom/prénom
-  const nameRegExp = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ- \-]{2,}$');
-  // Mise en place de la condition de validité pour les inputs nom/prénom
-  let msg = "Votre nom est valide";
-  if (nameRegExp.test(form.first.value) === false){
-    msg = "Entrez un nom valide";
+function isError(element, message) {
+  Msg.classList.add("messageErreur");
+  Msg.classList.remove("messageValid");
+  Msg.innerHTML = message;
+  if (element !== form.location) {
+    element.parentElement.append(Msg);
+  } else {
+    element[0].parentElement.append(Msg);
   }
-  console.log(msg);
-  console.log(form.first.value)
-  return inputName;
 }
 
-const validFamilyName = function(inputFamilyName){
-  const FamNameRegExp = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ- \-]{2,}$');
-
-  let msg = "Votre nom de Famille est valide";
-  if(FamNameRegExp.test(form.last.value) === false){
-    msg="Entrez votre nom de famille";
+function isValid(element, message, ) {
+  Msg.classList.add("messageValid");
+  Msg.classList.remove("messageErreur");
+  Msg.innerHTML = message;
+  if (element !== form.location) {
+    element.parentElement.append(Msg);
+  } else {
+    element[0].parentElement.append(Msg);
   }
-  console.log(msg);
-  return inputFamilyName;
 }
 
-const validEmail = function(inputEmail){
-  const emailRegExp = new RegExp ('^[a-zA-Z0-9À-ÖØ-öø-ÿ. \-\_]{1,}[@]{1}[a-zA-Z0-9.-_]{1,}[.]{1}[a-zA-Z]{2,10}$');
-  let msg ="Votre email est valide";
-
-  if (emailRegExp.test(form.email.value) === false){
-    msg = "Entrez une adresse mail valide"
-    return alert("Erreur Mail")
+function isInputError(element) {
+  for(element of formData){
+    if (element.children.value === false) {
+      element.setAttribute("data-error-visible", "true");
+    }
   }
-  return inputEmail;
+} 
 
-
-}
-
-const validDate = function(inputDate){
-  //On formate la date afin de l'avoir en format fr
-  let dt = new Date();
-  let dateL= dt.toLocaleString('fr-FR', {
-    weekday:"short",
-    month:"2-digit",
-    year:"numeric"
-  })
-  let msg = "Votre date de naissance est valide";
-  
-  // La condition nous sert à verifié que la date que rentre l'utilisateur n'est pas supérieur à la date d'aujourd'hui 
-  if (inputDate.value > dateL){
-    let msg="Vous devez entrer une date valide";
-  }
+function isInputSuccess(element){
  
 }
 
-const validTournament = function(inputTournament){
-  const tournamentRegExp = new RegExp('^[0-9]{1,}$');
-  let msg = "Votre chiffre est valide";
-  
-  if (tournamentRegExp.test(form.quantity.value) === false){
-    msg = "Veuillez entrer un chiffre valide";
-    return alert("Erreur blbk")
+
+// Création des fonctions afin de mettre en place la validité
+const validName = function () {
+  // Création de la regex pour les inputs nom/prénom
+  const nameRegExp = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ- -]{2,}$");
+  // Mise en place de la condition de validité pour les inputs nom/prénom
+  if (nameRegExp.test(form.first.value) === false) {
+    isError(form.first, "Votre prénom doit contenir au minimum deux caractères valide");
+    isInputError(form.first);
+
+    return false;
   }
-  return inputTournament;
-}
+  console.log(form.first.value);
+  isValid(form.first, "Votre prénom est valide");
+  isInputSuccess(form.first);
+  return true;
+};
 
-const validLocation = function(inputLocation){
-  let msg = "Vous avez cochez au moins un des propositions";
-
-  if (form.location.value === ""){
-    msg = "Vous devez cochez au minimum un lieu";
-    return alert("Erreur vous devez cochez au moins une proposition");
+const validFamilyName = function () {
+  const FamNameRegExp = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ- -]{2,}$");
+  if (FamNameRegExp.test(form.last.value) === false) {
+    isError(form.last, "Votre nom doit contenir au minimum deux caractères valide");
+    isInputError(form.last);
+    return false;
   }
-  console.log(msg);
-  return inputLocation;
-}
+  isValid(form.last, "Votre nom est valide");
+  isInputSuccess(form.last);
+  return true;
+};
 
-const validCondition = function(Condition){
-  let msg = "Vous avez accepté les conditions";
-  if(Condition.checked == false){
-    msg = "Vous devez accepter les termes";
-    return alert("Vous devez acceptez les termes")
+const validEmail = function () {
+  const emailRegExp = new RegExp(
+    "^[a-zA-Z0-9À-ÖØ-öø-ÿ. -_]{1,}[@]{1}[a-zA-Z0-9.-_]{1,}[.]{1}[a-zA-Z]{2,10}$"
+  );
+  if (emailRegExp.test(form.email.value) === false) {
+    isError(form.email, "Vous devez entrer une adresse email valide");
+    isInputError(form.email);
+    return false;
+
   }
-  return Condition;
-}
+  isValid(form.email, "Votre email est valide");
+  isInputSuccess(form.email);
 
-const validate = function(){
-  if((validName(form.first.value)) && (validEmail(form.email.value)) && (validTournament(form.quantity.value)) && (validLocation(form.location.value)) && (validCondition(Condition.checked === true)) === true){
-    alert("Félicitation votre réservation est effectué");
+  return true;
+};
+
+const validDate = function () {
+  const birthDate = new Date(form.birthdate.value);
+  const todayDate = new Date();
+
+  // La condition nous sert à verifié que la date que rentre l'utilisateur n'est pas supérieur à la date d'aujourd'hui
+  if (birthDate.getTime() > todayDate.getTime()) {
+    isError(form.birthdate, "Vous devez entrer votre date de naissance.");
+    isInputError(form.birthdate);
+    
+    return false;
+  }
+  isValid(form.birthdate, "Votre date de naissance est valide");
+  isInputSuccess(form.birthdate);
+
+  return true;
+};
+const validTournament = function () {
+  const tournamentRegExp = new RegExp("^[0-9]{1,}$");
+  if (tournamentRegExp.test(form.quantity.value) === false) {
+    isError(form.quantity, "Vous devez indiquer le nombre de tournois GameOn déjà joué");
+    isInputError(form.quantity);
+  }
+  isValid(form.quantity, "Vous avez renseigné votre nombre de tournois GameOn joué");
+  isInputSuccess(form.quantity);
+  console.log(form.quantity.value);
+  return true;
+};
+
+const validLocation = function () {
+  if (form.location.value === "") {
+    isError(form.location, "Veuillez cochez un des choix présent");
+
+    return false;
+  }
+  isValid(form.location, "Vous avez coché un des choix présent");
+
+  return true;
+};
+
+const validCondition = function () {
+  if (Condition.checked === false) {
+    isError(Condition, "Vous devez accepter les termes d'utilisation");
+    isInputError(Condition);
+
+    return false;
+  }
+  isValid(Condition, "Vous avez accepter les termes d'utilisation");
+  isInputSuccess(Condition);
+
+  return true;
+};
+
+const validate = function () {
+  if (
+    validName() &&
+    validFamilyName() &&
+    validEmail() &&
+    validDate() &&
+    validTournament() &&
+    validLocation() &&
+    validCondition()
+  ) {
     return true;
-    }
-    console.log(form.first.value);
-    console.log(form.last.value);
-    console.log(form.email.value);
-    console.log(form.quantity.value);
-    console.log(form.location.value);
-    console.log(Condition.checked);
-}
-
+  }
+};
